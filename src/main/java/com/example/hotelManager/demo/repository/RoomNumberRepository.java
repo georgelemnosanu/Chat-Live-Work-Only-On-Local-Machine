469f27d626a504f9c3725cf069d42909fb42f4cb
georgelemnosanu <georgelemnosanu@gmail.com>
2023-02-10 17:19:40 +0200

    bestVer

--- src/main/java/com/example/hotelManager/demo/repository/RoomNumberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.hotelManager.demo.repository;
-
-import com.example.hotelManager.demo.model.RoomNumber;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoomNumberRepository extends JpaRepository<RoomNumber,Long> {
-}
